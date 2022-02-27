876e447ed1670ce04ddf13512efc6a3f0679e00c
playteddypicker <hangyul010101@gmail.com>
2022-02-28 00:56:20 +0900

    3.5.3

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -11,7 +11,7 @@ module.exports = {
 
 		//나에게 dm하기
 		const teddypicker = await client.users.fetch('653157614452211712');
-		teddypicker.send(`봇이 **${guild.name}** 서버에 추가됨`);
+		teddypicker.send(`봇이 **${guild.id}@${guild.name}** 서버에 추가됨`);
 		teddypicker.send(`서버 수 : ${client.guilds.cache.size}개`);
 
 	}
